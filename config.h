edec468ec2942970984f606da23d215b22b0a091
dosisod <39638017+dosisod@users.noreply.github.com>
2020-07-18 20:58:37 -0700

    added slock, and music related keybinds

--- config.h
+++ config.h
@@ -58,6 +58,10 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *slockcmd[] = { "slock", NULL };
+static const char *musiccmd[] = { "/home/noot/git/dwm-stuff/song.sh", NULL };
+static const char *volupcmd[] = { "amixer", "sset", "Master", "5%+", NULL };
+static const char *voldowncmd[] = { "amixer", "sset", "Master", "5%-", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -68,8 +72,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
+	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.01} },
+	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.01} },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
@@ -94,6 +98,10 @@ static Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ MODKEY,                       XK_q,      spawn,          {.v = slockcmd } },
+	{ MODKEY,                       XK_backslash, spawn,       {.v = musiccmd } },
+	{ MODKEY|ShiftMask,             XK_bracketleft, spawn,     {.v = voldowncmd } },
+	{ MODKEY|ShiftMask,             XK_bracketright, spawn,    {.v = volupcmd } },
 };
 
 /* button definitions */
