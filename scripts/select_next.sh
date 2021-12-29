495aa1b788fbb0ea7078326f7a30d4e501a64bca
dosisod <39638017+dosisod@users.noreply.github.com>
2021-12-28 21:24:13 -0800

    Replace tempfile with mktemp

--- scripts/select_next.sh
+++ scripts/select_next.sh
@@ -5,7 +5,7 @@ songs=$("${SCRIPT_DIR}/select_song.sh")
 [ $? = 0 ] || exit 1
 [ "$songs" = "" ] && exit 1
 
-tmp=$(tempfile)
+tmp=$(mktemp)
 echo "$songs" | cat - /tmp/song_queue 2>/dev/null > $tmp
 mv $tmp /tmp/song_queue;
 
