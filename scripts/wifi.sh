783c7395af0a550ea4bcd1226bfcfc369d955482
dosisod <39638017+dosisod@users.noreply.github.com>
2022-01-24 17:59:03 -0800

    Add ability to connect to new wifi APs

--- scripts/wifi.sh
+++ scripts/wifi.sh
@@ -1,17 +1,21 @@
 #!/bin/sh
 
+msg() {
+	printf "" | dmenu -p "$1"
+}
+
 wifi_rescan() {
-	nmcli d wifi rescan
+	nmcli device wifi rescan
 }
 
 wifi_reboot() {
 	nmcli radio wifi off
 	nmcli radio wifi on
-	printf "" | dmenu -p "Done"
+	msg "Done"
 }
 
 wifi_connect() {
-	wifi=$(nmcli c | tail -n +2 | dmenu -i -l -1)
+	wifi=$(nmcli connection | tail -n +2 | dmenu -i -l -1)
 	[ -z "$wifi" ] && {
 		main;
 		exit 0;
@@ -19,30 +23,59 @@ wifi_connect() {
 
 	uuid=$(printf "$wifi" | grep -o "........-....-....-....-............")
 
-	err=$(nmcli c up "$uuid" 2>&1)
+	err=$(nmcli connection up "$uuid" 2>&1)
 	[ "$?" = "4" ] && {
-		dmenu -p "$err"
+		msg "$err"
 		exit 1
 	}
 
-	printf "" | dmenu -p "Connection successful"
+	msg "Connection successful"
+}
+
+wifi_new_connection() {
+	wifi=$(wifi_list)
+
+	[ -z "$wifi" ] && {
+		main;
+		exit 0;
+	}
+
+	bssid=$(printf "$wifi" | grep -o "..:..:..:..:..:..")
+	password=$(msg "Password")
+
+	expr "${#password}" ">" 8 > /dev/null
+	[ "$?" = "1" ] && {
+		msg "Supplied password was too short"
+		main;
+		exit 0;
+	}
+
+	nmcli device wifi connect "$bssid" password "$password"
+
+	[ "$?" = "0" ] || {
+		msg "Could not make connection"
+		exit 1;
+	}
+
+	msg "Connection successful"
 }
 
 wifi_list() {
 	wifi_rescan
-	nmcli d wifi list | dmenu -i -l -1
+	nmcli device wifi list | dmenu -i -l -1
 }
 
 main() {
-	action=$(printf "Connect\nReboot\nRescan\nList\n" | dmenu -i -l -1 -p "wifi")
+	action=$(printf "Connect\nNew Connection\nReboot\nRescan\nList\n" | dmenu -i -l -1 -p "wifi")
 	[ -z "$action" ] && exit 0
 
 	case "$action" in
 		Connect) wifi_connect ;;
+		"New Connection") wifi_new_connection ;;
 		Reboot) wifi_reboot ;;
 		Rescan) wifi_rescan ;;
 		List) wifi_list ;;
-		*) dmenu -p "Invalid option" ; exit 1 ;;
+		*) msg "Invalid option" ; exit 1 ;;
 	esac
 }
 
