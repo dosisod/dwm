a25cd53b5d1c35c633b1d965152eb81eb032b9c9
dosisod <39638017+dosisod@users.noreply.github.com>
2022-05-16 20:52:52 -0700

    Fix timer not killing timer if it is already running

--- scripts/timer.sh
+++ scripts/timer.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+timer_file=/tmp/timer
+
 hours=$(echo -n "0" | dmenu -p "HOURS")
 [ -z "$hours" ] && exit 1
 
@@ -11,8 +13,11 @@ seconds=$(echo -n "0" | dmenu -p "SECONDS")
 
 offset=$(expr "(" $hours '*' 3600 ")" + "(" $minutes '*' 60 ")" + $seconds)
 
+kill $(pgrep "timer.sh" | grep -v $$)
+rm -f "$timer_file"
+
 while true; do
-	echo "$(date -u --date="@$offset" +%T)" > /tmp/timer
+	echo "$(date -u --date="@$offset" +%T)" > "$timer_file"
 	offset=$(expr $offset - 1)
 
 	sleep 1
@@ -20,5 +25,5 @@ while true; do
 	expr $offset "<" 0 && break
 done
 
-rm /tmp/timer
+rm "$timer_file"
 echo "" | dmenu -p "Times up!"
